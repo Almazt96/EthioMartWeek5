ebbcb1aba120e3e7957de51a60366ab40fc6b122
Almaz <Almazt96@gmail.com>
2025-01-17 01:05:01 +0300

    Before start of fine tunining

--- notebooks/Data Ingestion Pipeline.py
+++ notebooks/Data Ingestion Pipeline.py
@@ -20,7 +20,7 @@ client.start()
 print("Client connected successfully!")
 
 # Fetch messages from a channel
-def fetch_channel_data(channel_username, limit=100):
+def fetch_channel_data(channel_username, limit=50):
     messages = []
     for message in client.iter_messages(channel_username, limit=limit):
         if message.text:
@@ -34,13 +34,13 @@ def preprocess_amharic_text(df):
     return df
 
 # Fetch data from Telegram channel
-channel_data = fetch_channel_data('qnashcom')
+channel_data = fetch_channel_data('meneshayeofficial')
 
 # Preprocess data
 clean_data = preprocess_amharic_text(channel_data)
 
 # Save data to CSV
-clean_data.to_csv(r'C:\Users\Almazt\OneDrive - Ethiopian Airlines\Desktop\10 Academy\EthioMart-Week 5\data/telegram_qnashcom_data.csv', index=False)
+clean_data.to_csv(r'C:\Users\Almazt\OneDrive - Ethiopian Airlines\Desktop\10 Academy\EthioMart-Week 5\data/telegram_meneshayeofficial_clean_data.csv', index=False)
 
 # Disconnect from Telegram client
 client.disconnect()
